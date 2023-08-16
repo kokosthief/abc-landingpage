59db4f0424fc60c7e6b6a53370104c2794c36841
hrwillmott <hello@hrwillmott.com>
2023-08-16 16:54:31 +0200

    Update

--- src/components/Vision.jsx
+++ src/components/Vision.jsx
@@ -11,7 +11,11 @@ const FeatureCard = ({ icon, title, content, index }) => (
     <div
       className={`w-[64px] h-[64px] rounded-full ${styles.flexCenter} bg-dimBlue`}
     >
-      <img src={icon} alt="star" className="w-[50%] h-[50%]  object-contain" />
+      <img
+        src={icon}
+        alt="star"
+        className="fill-white w-[50%] h-[50%]  object-contain"
+      />
     </div>
     <div className="flex-1 flex flex-col ml-3">
       <h4 className="font-poppins font-semibold text-white text-[18px] leading-[23.4px] mb-1">

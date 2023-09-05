920ce309896b5ce01d4211b21db3c44463cfe22b
hrwillmott <hello@hrwillmott.com>
2023-09-05 14:48:04 +0200

    Fixed buttons

--- src/components/HowToUse.jsx
+++ src/components/HowToUse.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect } from "react";
+import { telegramcircle } from "../assets";
 
 const HowToUse = () => {
   useEffect(() => {
@@ -27,50 +28,92 @@ const HowToUse = () => {
 
                   <p className="font-spacegrotesk font-normal text-sm sm:text-lg  text-[#0a0a0a]">
                     <ol className="list-inside list-decimal ">
-                      <li class="block w-full cursor-default px-4 my-1  font-semibold focus:outline-none focus:ring-0">
+                      <li class="block w-full cursor-default px-4 my-1 pb-1  font-semibold focus:outline-none focus:ring-0">
                         1. Install AlphaBot in Telegram or Discord
                       </li>
-                      <div className=" inline-flex gap-5 ml-5">
-                        <a
-                          class="group relative inline-block"
-                          href="https://discord.com/api/oauth2/authorize?client_id=1141380686188662884&permissions=534723951680&scope=bot"
-                        >
-                          <div class="flex absolute -left-3 items-center h-full">
-                            <div class="h-5 scale-0 opacity-0 group-hover:opacity-100 group-hover:scale-100 w-1 transition-all duration-200 origin-left bg-white rounded-r"></div>
-                          </div>
-                          <div class="group-active:translate-y-px">
-                            <div class="text-gray-100 group-hover:rounded-2xl group-hover:bg-[#5D65F3] group-hover:text-white bg-gray-700 rounded-3xl flex items-center justify-center w-12 h-12 transition-all duration-200 overflow-hidden">
-                              <svg
-                                aria-hidden="false"
-                                aria-label="Discord"
-                                viewBox="0 0 28 20"
-                                class="w-7 h-5"
-                              >
-                                <path
-                                  fill="currentColor"
-                                  d="M23.021 1.677A21.227 21.227 0 0017.658 0c-.252.462-.483.935-.687 1.418a19.931 19.931 0 00-5.943 0C10.82.935 10.59.462 10.337.005c-1.856.32-3.659.88-5.37 1.677C1.567 6.78.65 11.754 1.111 16.652A21.504 21.504 0 007.691 20c.532-.726 1.004-1.5 1.407-2.309a13.582 13.582 0 01-2.221-1.078c.188-.137.37-.274.547-.428a15.232 15.232 0 0013.152 0c.177.148.36.291.541.428-.707.424-1.453.787-2.22 1.078.408.808.875 1.578 1.405 2.303a21.5 21.5 0 006.58-3.347h.007c.541-5.674-.922-10.6-3.868-14.97zM9.681 13.638c-1.283 0-2.34-1.193-2.34-2.644S8.37 8.35 9.68 8.35c1.308 0 2.359 1.193 2.338 2.644 0 1.451-1.036 2.644-2.339 2.644zm8.635 0c-1.283 0-2.34-1.193-2.34-2.644s1.036-2.644 2.34-2.644c1.302 0 2.36 1.193 2.338 2.644 0 1.451-1.036 2.644-2.338 2.644z"
-                                ></path>
-                              </svg>
+                      <div className="flex flex-col xs:flex-row gap-2 sm:gap-4 sm:pl-5 ">
+                        <div className="flex mx-auto sm:mx-0">
+                          <a
+                            class="group relative "
+                            href="https://discord.com/api/oauth2/authorize?client_id=1141380686188662884&permissions=534723951680&scope=bot"
+                          >
+                            <div class="flex absolute -left-3 items-center h-full">
+                              <div class="h-5 scale-0 opacity-0 group-hover:opacity-100 group-hover:scale-100 w-1 transition-all duration-200 origin-left bg-white rounded-r"></div>
                             </div>
-                          </div>
-                        </a>
 
-                        <a
-                          href="https://t.me/ABCAlphaBot?startgroup=true"
-                          target="_blank"
-                          className="telegramim_button -ml-2"
-                          style={{
-                            fontSize: "16px",
-                            width: "116px",
-                            background: "#27A5E7",
-                            boxShadow: "1px 1px 3px #27A5E7",
-                            color: "#FFFFFF",
-                            borderRadius: "46px",
-                          }}
-                          title="Telegram Button"
-                        >
-                          <i></i> Telegram
-                        </a>
+                            <div class="group-active:translate-y-px">
+                              <div class="text-gray-100 group-hover:rounded-2xl group-hover:bg-[#5D65F3] group-hover:text-white bg-gray-700 rounded-3xl flex items-center justify-center w-36 h-12 transition-all duration-200 overflow-hidden shadow-lg">
+                                <svg
+                                  aria-hidden="false"
+                                  aria-label="Discord"
+                                  viewBox="0 0 28 20"
+                                  class="w-7 h-5 mr-1"
+                                >
+                                  <path
+                                    fill="currentColor"
+                                    d="M23.021 1.677A21.227 21.227 0 0017.658 0c-.252.462-.483.935-.687 1.418a19.931 19.931 0 00-5.943 0C10.82.935 10.59.462 10.337.005c-1.856.32-3.659.88-5.37 1.677C1.567 6.78.65 11.754 1.111 16.652A21.504 21.504 0 007.691 20c.532-.726 1.004-1.5 1.407-2.309a13.582 13.582 0 01-2.221-1.078c.188-.137.37-.274.547-.428a15.232 15.232 0 0013.152 0c.177.148.36.291.541.428-.707.424-1.453.787-2.22 1.078.408.808.875 1.578 1.405 2.303a21.5 21.5 0 006.58-3.347h.007c.541-5.674-.922-10.6-3.868-14.97zM9.681 13.638c-1.283 0-2.34-1.193-2.34-2.644S8.37 8.35 9.68 8.35c1.308 0 2.359 1.193 2.338 2.644 0 1.451-1.036 2.644-2.339 2.644zm8.635 0c-1.283 0-2.34-1.193-2.34-2.644s1.036-2.644 2.34-2.644c1.302 0 2.36 1.193 2.338 2.644 0 1.451-1.036 2.644-2.338 2.644z"
+                                  ></path>
+                                </svg>
+                                <span className="align-middle ml-1 text-md font-sans">
+                                  Discord
+                                </span>
+                              </div>
+                            </div>
+                          </a>
+                        </div>
+
+                        <div className="mx-auto sm:mx-0 ">
+                          <a
+                            href="https://t.me/ABCAlphaBot?startgroup=true"
+                            target="_blank"
+                            className="flex flex-row  justify-center  items-center  h-12"
+                            style={{
+                              fontSize: "16px",
+                              width: "146px",
+                              background: "#27A5E7",
+                              boxShadow: "1px 1px 3px #27A5E7",
+                              color: "#FFFFFF",
+                              borderRadius: "46px",
+                              padding: "10px", // Add padding for spacing
+                            }}
+                            title="Telegram Button"
+                          >
+                            <svg
+                              xmlns="http://www.w3.org/2000/svg"
+                              width="45"
+                              height="45"
+                              viewBox="0 0 256 256"
+                              className="inline-flex"
+                            >
+                              <defs>
+                                <linearGradient
+                                  id="logosTelegram0"
+                                  x1="50%"
+                                  x2="50%"
+                                  y1="0%"
+                                  y2="100%"
+                                >
+                                  <stop offset="0%" stop-color="#2AABEE" />
+                                  <stop offset="100%" stop-color="#229ED9" />
+                                </linearGradient>
+                              </defs>
+                              <path
+                                fill="url(#logosTelegram0)"
+                                d="M128 0C94.06 0 61.48 13.494 37.5 37.49A128.038 128.038 0 0 0 0 128c0 33.934 13.5 66.514 37.5 90.51C61.48 242.506 94.06 256 128 256s66.52-13.494 90.5-37.49c24-23.996 37.5-56.576 37.5-90.51c0-33.934-13.5-66.514-37.5-90.51C194.52 13.494 161.94 0 128 0Z"
+                              />
+                              <path
+                                fill="#FFF"
+                                d="M57.94 126.648c37.32-16.256 62.2-26.974 74.64-32.152c35.56-14.786 42.94-17.354 47.76-17.441c1.06-.017 3.42.245 4.96 1.49c1.28 1.05 1.64 2.47 1.82 3.467c.16.996.38 3.266.2 5.038c-1.92 20.24-10.26 69.356-14.5 92.026c-1.78 9.592-5.32 12.808-8.74 13.122c-7.44.684-13.08-4.912-20.28-9.63c-11.26-7.386-17.62-11.982-28.56-19.188c-12.64-8.328-4.44-12.906 2.76-20.386c1.88-1.958 34.64-31.748 35.26-34.45c.08-.338.16-1.598-.6-2.262c-.74-.666-1.84-.438-2.64-.258c-1.14.256-19.12 12.152-54 35.686c-5.1 3.508-9.72 5.218-13.88 5.128c-4.56-.098-13.36-2.584-19.9-4.708c-8-2.606-14.38-3.984-13.82-8.41c.28-2.304 3.46-4.662 9.52-7.072Z"
+                              />
+                            </svg>
+                            <span className="font-sans text-md ">Telegram</span>
+                          </a>
+                        </div>
+                        {/* <img
+                              src={telegramcircle}
+                              alt="CredibleCallers"
+                              className="w-[25px] h-auto inline-block mr-1"
+                            /> */}
                       </div>
                       <li class="block w-full cursor-default px-4 py-2 my-1  font-semibold focus:outline-none focus:ring-0">
                         2. Ensure AlphaBot is assigned proper permissions in{" "}

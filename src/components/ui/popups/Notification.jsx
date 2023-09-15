36b6365c8616614ab39d3f4f2b52de24d73bc41a
Andrew Hrechkan <andriihrechkan@gmail.com>
2023-09-15 15:33:52 +0300

    rewrote the modal notification window

--- src/components/ui/popups/Notification.jsx
+++ src/components/ui/popups/Notification.jsx
@@ -6,7 +6,7 @@ export const Notification = ({ isOpened, onClose, header, message }) => {
       header={header}
       isOpened={isOpened}
       onClose={onClose}
-      className="max-w-[500px]"
+      className="ss:!max-w-[500px] ss:!min-w-[auto] !min-w-[90%] !max-w-[90%]"
     >
       <p className="body mt-1 text-neutral-black">{message}</p>
     </MainPopup>
